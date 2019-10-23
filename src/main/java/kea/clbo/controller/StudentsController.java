5edcdb8aa5311ea0ae4e13fd54c8cd347980ec25
clbokea <clbo@kea.dk>
2019-10-23 13:38:28 +0200

    before 2

--- src/main/java/kea/clbo/controller/StudentsController.java
+++ src/main/java/kea/clbo/controller/StudentsController.java
@@ -4,6 +4,8 @@ import kea.clbo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,18 @@ public class StudentsController {
     public String index(Model model){
         model.addAttribute("students_data", students);
         return "index";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Student student){ // @ModelAttribute("name") String name
+        student.setId(students.size()+1);
+        students.add(student);
+        return "redirect:/";
     }
 
     @GetMapping("/about")
--- src/main/java/kea/clbo/model/Student.java
+++ src/main/java/kea/clbo/model/Student.java
@@ -5,8 +5,9 @@ public class Student {
     private int id;
     private String name;
 
-    public Student(int id, String name) {
+    public Student(){}
 
+    public Student(int id, String name) {
         this.id = id;
         this.name =name;
     }
